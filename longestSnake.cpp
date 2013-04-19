02c031c966bf77a06c022288c4abc417f219ba4e
jiabailie <spotlightyrg@gmail.com>
2013-04-19 13:33:56 +0800

    Update longestSnake.cpp

--- longestSnake.cpp
+++ longestSnake.cpp
@@ -1,3 +1,19 @@
+/*
+You are given a 2-D array with same number of rows and columns. You have to determine the longest snake in the array. 
+The property to find the snake is the difference between the adjacent(left, right, up or down) should be either 1 or -1. 
+If there are more than one snakes with maximum length, the output should print both of them. 
+
+Example--> 
+
+The given array elements are as follows: 
+
+4    7	9	8 
+5	6	5	4 
+6	7	8	5 
+10	9	7	6 
+
+The longest snakes are 7->6->5->4->5->6 and 7->6->7->6->5->4
+ */
 #include <cstdio>
 #include <cstring>
 #include <climits>
@@ -8,7 +24,10 @@ using namespace std;
 
 int n = 0;
 int board[MAX][MAX];
+bool g_visited[MAX][MAX];
 vector<vector<int> > res;
+vector<vector<int> > g_px;
+vector<vector<int> > g_py;
 
 int mlength = INT_MIN;
 int dx[4] = {1, 0, -1, 0};
@@ -16,7 +35,7 @@ int dy[4] = {0, 1, 0, -1};
 
 inline int abs(int x) { return x > 0 ? x : -x; }
 
-void dfs(int x, int y, vector<int> current, bool visited[][MAX])
+void dfs(int x, int y, vector<int> current, vector<int> px, vector<int> py, bool visited[][MAX])
 {
     int change = 0, i = 0;
     for(i = 0; i < 4; i++)
@@ -29,11 +48,15 @@ void dfs(int x, int y, vector<int> current, bool visited[][MAX])
             change = 1;
             visited[x + dx[i]][y + dy[i]] = true;
             current.push_back(board[x + dx[i]][y + dy[i]]);
-            
-            dfs(x + dx[i], y + dy[i], current, visited);
+            px.push_back(x + dx[i]);
+            py.push_back(y + dy[i]);
+
+            dfs(x + dx[i], y + dy[i], current, px, py, visited);
 
             visited[x + dx[i]][y + dy[i]] = false;
-  	    current.pop_back();
+            current.pop_back();
+            px.pop_back();
+            py.pop_back();
         }
     }
     if(!change && current.size() > 0)
@@ -43,9 +66,13 @@ void dfs(int x, int y, vector<int> current, bool visited[][MAX])
             if((int)current.size() > mlength)
             {
                 res.clear();
+                g_px.clear();
+                g_py.clear();
                 mlength = current.size();
             }
             res.push_back(current);
+            g_px.push_back(px);
+            g_py.push_back(py);
             for(i = 0; i < (int)current.size(); i++)
             {
                 printf("%d ", current[i]);
@@ -55,20 +82,46 @@ void dfs(int x, int y, vector<int> current, bool visited[][MAX])
     }
 }
 
+bool checkpath(int x, int y)
+{
+    bool ret = false;
+    for(int i = 0; i < (int)g_px.size() && !ret; i++)
+    {
+        for(int j = 0; j < (int)g_px[i].size() && !ret; j++)
+        {
+            if(g_px[i][j] == x && g_py[i][j] == y)
+            {
+                ret = true;
+            }
+        }
+    }
+    return ret;
+}
 int find()
 {
-    int i = 0, j = 0, k = 0;
+    int i = 0, j = 0;
     vector<int> current;
+    vector<int> px;
+    vector<int> py;
     bool visited[MAX][MAX];
     for(i = 0; i < n; i++)
     {
         for(j = 0; j < n; j++)
         {
-            current.clear();
-            memset(visited, 0, sizeof(visited));
-            visited[i][j] = true;
-            current.push_back(board[i][j]);
-            dfs(i, j, current, visited);
+            if(!checkpath(i, j))
+            {
+                current.clear();
+				px.clear();
+				py.clear();
+				memset(visited, 0, sizeof(visited));
+
+				visited[i][j] = true;
+
+				current.push_back(board[i][j]);
+				px.push_back(i);
+				py.push_back(j);
+				dfs(i, j, current, px, py, visited);
+            }
         }
     }
     return 0;
