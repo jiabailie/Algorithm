8c2e7a7df4fc4d2fa20d51c0fd3c73c2d4dbef1d
jiabailie <spotlightyrg@gmail.com>
2013-05-04 14:53:31 +0800

    Update 13050403.cpp

--- 13050403.cpp
+++ 13050403.cpp
@@ -20,12 +20,12 @@ Explanation - Following are the possible combinations for team score of 8
 #include <iostream>
 using namespace std;
 
-int find(int n)
+int find1(int n)
 {
     int i = 0, j = 0, ret = 0;
-    for(i = 0; i <= (n + 2) / 3; i++)
+    for(i = 0; i <= n / 3; i++)
     {
-        for(j = 0; j <= (n + 1) / 2; j++)
+        for(j = 0; j <= n / 2; j++)
         {
             if(i * 3 + j * 2 <= n)
             {
@@ -36,10 +36,20 @@ int find(int n)
     return ret;
 }
 
+int find2(int n)
+{
+    int i = 0, ret = 0;
+    for(i = 0; i <= n / 3; i++)
+    {
+        ret += 1 + (n - 3 * i) / 2;
+    }
+    return ret;
+}
+
 int main()
 {
     int n = 0;
     cin >> n;
-    cout << find(n);
+    cout << find1(n) << " " << find2(n);
     return 0;
 }
