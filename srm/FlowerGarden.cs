85c04d4dda306afd85cb90df8ae98c4e69c60352
jiabailie <spotlightyrg@gmail.com>
2013-08-19 22:54:50 +0800

    Signed-off-by: jiabailie <spotlightyrg@gmail.com>

---
+++ srm/FlowerGarden.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class FlowerGarden
+{
+    public int[] getOrdering(int[] height, int[] bloom, int[] wilt)
+    {
+        int i = 0, j = 0, k = 0;
+        int n = height.Length;
+        bool[] done = new bool[n];
+        int[] res = new int[n];
+
+        for (i = 0; i < n; i++)
+        {
+            int best = -1;
+            for (j = 0; j < n; j++)
+            {
+                if (!done[j])
+                {
+                    bool ok = true;
+                    for (k = 0; k < n; k++)
+                    {
+                        if (k != j && 
+                            !done[k] && 
+                            height[k] < height[j] && 
+                            bloom[k] <= wilt[j] && 
+                            wilt[k] >= bloom[j])
+                        {
+                            ok = false;
+                        }
+                    }
+                    if (ok && (best == -1 || height[j] > height[best]))
+                    {
+                        best = j;
+                    }
+                }
+            }
+            res[i] = height[best];
+            done[best] = true;
+        }
+
+        return res;
+    }
+}
+
---
+++ srm/TestFunction.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab
+{
+    public class TestFunction
+    {
+        /// <summary>
+        /// Transform one binary string to an unsigned long type integer.
+        /// </summary>
+        /// <param name="bitstring"></param>
+        /// <returns></returns>
+        public static ulong TransferstringToUnsignedLongInteger(string bitstring)
+        {
+            int i = 0;
+            int len = bitstring.Length;
+            ulong ret = 0;
+
+            for (i = 0; i < len; i++)
+            {
+                ret <<= 1;
+                if (bitstring[i] == '1')
+                {
+                    ret++;
+                }
+            }
+            return ret;
+        }
+
+        public static string TransferUnsignedLongIntegerToString(ulong number)
+        {
+            ulong index = ((ulong)1 << 63);
+            StringBuilder sbRet = new StringBuilder();
+
+            while (index > 0)
+            {
+                sbRet.Append(((number & index) == 0 ? "0" : "1"));
+                index >>= 1;
+            }
+            return sbRet.ToString();
+        }
+
+        /// <summary>
+        /// The subBlock is a matrix size of [height, width].
+        /// This function transforms the matrix to a ulong type array.
+        /// </summary>
+        /// <param name="subBlock"></param>
+        /// <returns></returns>
+        public static ulong[] TransferOneZerostringToUlongArray(string subBlock)
+        {
+            ulong[] ret = null;
+            int i = 0, arrayLength = 0;
+            int length = subBlock.Length + 1;
+
+            subBlock = "1" + subBlock;
+
+            arrayLength = length / 64;
+
+            if (length % 64 != 0)
+            {
+                arrayLength++;
+                length = (arrayLength << 6);
+
+                subBlock = subBlock.PadRight(length, '0');
+            }
+            ret = new ulong[arrayLength];
+            for (i = 0; i < arrayLength; i++)
+            {
+                ret[i] = TransferstringToUnsignedLongInteger(subBlock.Substring(i << 6, 64));
+            }
+
+            return ret;
+        }
+    }
+}
