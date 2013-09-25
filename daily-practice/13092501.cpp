96256b3e5ce7d590c3be7d762b6c935df87516d1
jiabailie <spotlightyrg@gmail.com>
2013-09-25 14:08:15 +0800

    Update 13092501.cpp

--- daily-practice/13092501.cpp
+++ daily-practice/13092501.cpp
@@ -153,7 +153,7 @@ inline void deleteMatrix(int** matrix)
 int main()
 {
 	int i = 0, j = 0;
-	//n = (rand() % 10) + 10;
+	n = (rand() % 10) + 10;
 	int** matrix = (int**)malloc(sizeof(int*) * n);
 	for(i = 0; i < n; i++)
 	{
