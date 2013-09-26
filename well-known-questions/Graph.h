f59b318675cd99ea1cf0deea18d5b317a028463c
jiabailie <spotlightyrg@gmail.com>
2013-09-26 10:22:20 +0800

    Update Graph.h

--- well-known-questions/Graph.h
+++ well-known-questions/Graph.h
@@ -15,6 +15,19 @@ enum GraphTypes
 	Directed = 1
 };
 
+struct Edge
+{
+	int node1;
+	int node2;
+	int weight;
+	Edge() : node1(0), node2(0), weight(-1) {}
+	Edge(int _node1, int _node2, int _weight) : node1(_node1), node2(_node2), weight(_weight) {}
+	Edge(const Edge& _edge);
+	Edge& operator=(const Edge& _edge);
+	void partCopy(Edge& edge, const Edge& _edge);
+	~Edge() {}
+};
+
 struct Graph
 {
 	int n; // The amount of different nodes.
