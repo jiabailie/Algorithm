6ee93fefb02cb57bc4979f9811065d5438e7f2d8
jiabailie <spotlightyrg@gmail.com>
2013-10-26 00:13:17 +0800

    Signed-off-by: jiabailie <spotlightyrg@gmail.com>

---
+++ data-structure/PriorityQueue/2013102506.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+#include <iostream>
+#include "PriorityQueue.h"
+
+class node
+{
+public:
+	int key;
+	int value;
+	node() : key(0), value(0) {}
+	node(const int _key, const int _value) { key = _key; value = _value; }
+};
+
+int main()
+{
+	int k = 0, i = 0;
+	const int n = 10;
+	PriorityQueue<node> pq;
+
+	for(i = 0; i < n; i++)
+	{
+		k = rand() % 1000;
+		Single<node>* single = new Single<node>(k, new node(k, k * 100));
+		pq.push(single);
+	}
+
+	while(!pq.empty())
+	{
+		Single<node>* single = pq.pop();
+		cout << single->key << " ";
+	}
+	cout << endl;
+	return 0;
+}
---
+++ data-structure/PriorityQueue/PriorityQueue.h
@@ -0,0 +1,116 @@
+#pragma once
+#include <vector>
+#include <map>
+#include <cstring>
+
+using namespace std;
+
+inline int HeapLeft(int p) { return (p << 1) + 1; }
+
+inline int HeapRight(int p) { return (p + 1) << 1; }
+
+template<typename T>
+class Single
+{
+public:
+	int key;
+	T* value;
+
+	Single() {}
+
+	Single(const int _key, const T* _value)
+	{
+		//if(!value) { value = new T; }
+
+		key = _key;
+		value = new T;
+		memcpy(value, _value, sizeof(T));
+	}
+
+	Single(const Single& _single)
+	{
+		if(&_single == this) { return; }
+		Single(_single.key, _single.value);
+	}
+
+	Single& operator=(const Single& _single)
+	{
+		if(&_single == this) { return *this; }
+		Single(_single.key, _single.value);
+		return *this;
+	}
+
+	~Single()
+	{
+		delete value;
+	}
+};
+
+template<typename T>
+inline void iSwap(Single<T>*& a, Single<T>*& b)
+{
+	Single<T>* c = a;
+	a = b;
+	b = c;
+}
+
+template<typename T>
+class PriorityQueue
+{
+private:
+	vector<Single<T>*> queue;
+public:
+	PriorityQueue(void) {};
+
+	~PriorityQueue(void) {};
+
+	bool empty() { return queue.empty(); }
+
+	unsigned int size() { return queue.size(); }
+
+	Single<T>* pop()
+	{
+		if(queue.empty())
+		{
+			return 0;
+		}
+		Single<T>* top = queue[0];
+		queue.erase(queue.begin());
+		if(!queue.empty())
+		{
+			build();
+		}
+		return top;
+	}
+
+	void push(Single<T>* elem)
+	{
+		bool empty = queue.empty();
+		queue.push_back(elem);
+		if(empty)
+		{		
+			return;
+		}
+		build();
+	}
+
+	void build()
+	{
+		int len = queue.size();
+	
+		int left = 0;
+		int right = 0;
+		int qmax = 0;
+		for(int i = (len + 1) >> 1; i >= 0; i--)
+		{
+			qmax = i;
+			left = HeapLeft(i);
+			right = HeapRight(i);
+	
+			if(left < len && queue[left]->key > queue[qmax]->key) { qmax = left; }
+			if(right < len && queue[right]->key > queue[qmax]->key) { qmax = right; }
+	
+			if(qmax != i) { iSwap(queue[i], queue[qmax]); }
+		}
+	}
+};
