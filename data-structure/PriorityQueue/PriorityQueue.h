c12cf59967dad652d4c6ad94609d05c2854b648e
jiabailie <spotlightyrg@gmail.com>
2013-10-28 09:39:44 +0800

    Update PriorityQueue.h

--- data-structure/PriorityQueue/PriorityQueue.h
+++ data-structure/PriorityQueue/PriorityQueue.h
@@ -13,104 +13,102 @@ template<typename T>
 class Single
 {
 public:
-	int key;
-	T* value;
+        int key;
+        T* value;
 
-	Single() {}
+        Single() {}
 
-	Single(const int _key, const T* _value)
-	{
-		//if(!value) { value = new T; }
+        Single(const int _key, const void* _value)
+        {
+                key = _key;
+                value = (T*)_value;  // set the current pointer points at the address of _value
+				_value = 0;			 // free the original pointer
+        }
 
-		key = _key;
-		value = new T;
-		memcpy(value, _value, sizeof(T));
-	}
+        Single(const Single& _single)
+        {
+                if(&_single == this) { return; }
+                Single(_single.key, _single.value);
+        }
 
-	Single(const Single& _single)
-	{
-		if(&_single == this) { return; }
-		Single(_single.key, _single.value);
-	}
+        Single& operator=(const Single& _single)
+        {
+                if(&_single == this) { return *this; }
+                Single(_single.key, _single.value);
+                return *this;
+        }
 
-	Single& operator=(const Single& _single)
-	{
-		if(&_single == this) { return *this; }
-		Single(_single.key, _single.value);
-		return *this;
-	}
-
-	~Single()
-	{
-		delete value;
-	}
+        ~Single()
+        {
+                delete value;
+        }
 };
 
 template<typename T>
 inline void iSwap(Single<T>*& a, Single<T>*& b)
 {
-	Single<T>* c = a;
-	a = b;
-	b = c;
+        Single<T>* c = a;
+        a = b;
+        b = c;
 }
 
 template<typename T>
 class PriorityQueue
 {
 private:
-	vector<Single<T>*> queue;
+        vector<Single<T>*> queue;
 public:
-	PriorityQueue(void) {};
+        PriorityQueue(void) {};
 
-	~PriorityQueue(void) {};
+        ~PriorityQueue(void) {};
 
-	bool empty() { return queue.empty(); }
+        bool empty() { return queue.empty(); }
 
-	unsigned int size() { return queue.size(); }
+        unsigned int size() { return queue.size(); }
 
-	Single<T>* pop()
-	{
-		if(queue.empty())
-		{
-			return 0;
-		}
-		Single<T>* top = queue[0];
-		queue.erase(queue.begin());
-		if(!queue.empty())
-		{
-			build();
-		}
-		return top;
-	}
+        Single<T>* pop()
+        {
+                if(queue.empty())
+                {
+                        return 0;
+                }
+                Single<T>* top = queue[0];
+                queue.erase(queue.begin());
+                if(!queue.empty())
+                {
+                        build();
+                }
+                return top;
+        }
 
-	void push(Single<T>* elem)
-	{
-		bool empty = queue.empty();
-		queue.push_back(elem);
-		if(empty)
-		{		
-			return;
-		}
-		build();
-	}
+        void push(Single<T>* elem)
+        {
+                bool empty = queue.empty();
+                queue.push_back(elem);
+                if(empty)
+                {                
+                        return;
+                }
+                build();
+        }
 
-	void build()
-	{
-		int len = queue.size();
-	
-		int left = 0;
-		int right = 0;
-		int qmax = 0;
-		for(int i = (len + 1) >> 1; i >= 0; i--)
-		{
-			qmax = i;
-			left = HeapLeft(i);
-			right = HeapRight(i);
-	
-			if(left < len && queue[left]->key > queue[qmax]->key) { qmax = left; }
-			if(right < len && queue[right]->key > queue[qmax]->key) { qmax = right; }
-	
-			if(qmax != i) { iSwap(queue[i], queue[qmax]); }
-		}
-	}
+        void build()
+        {
+                int len = queue.size();
+        
+                int left = 0;
+                int right = 0;
+                int qmax = 0;
+                for(int i = (len + 1) >> 1; i >= 0; i--)
+                {
+                        qmax = i;
+                        left = HeapLeft(i);
+                        right = HeapRight(i);
+        
+                        if(left < len && queue[left]->key > queue[qmax]->key) { qmax = left; }
+                        if(right < len && queue[right]->key > queue[qmax]->key) { qmax = right; }
+        
+                        if(qmax != i) { iSwap(queue[i], queue[qmax]); }
+                }
+        }
 };
