f642e2958e217fd0f16a7609ba4555fd25f23719
jiabailie <spotlightyrg@gmail.com>
2013-10-31 10:18:23 +0800

    Remove one redundant function and modify the structure to process the array from head to tail instead of after reversing the arrays.

--- srm/SRM595.1000.LittleElephantAndXor.cs
+++ srm/SRM595.1000.LittleElephantAndXor.cs
@@ -5,45 +5,37 @@ using System.Text;
 public class LittleElephantAndXor
 {
     private int len = 32;
-    private string sa = null;
-    private string sb = null;
-    private string sc = null;
+    private char[] sa = null;
+    private char[] sb = null;
+    private char[] sc = null;
     private long[, , ,] dp = null;
-
-    private string reverse(string s)
-    {
-        string ret = "";
-        for (int i = s.Length - 1; i >= 0; i--) { ret += s[i]; }
-        return ret;
-    }
 
     private long solve(int t, int eA, int eB, int eC)
     {
-        int i = 0, j = 0, k = 0;
         if (dp[t, eA, eB, eC] == -1)
         {
             dp[t, eA, eB, eC] = 0;
-            if (t == 0)
+            if (t == len - 1)
             {
                 dp[t, eA, eB, eC] = 1;
             }
             else
             {
                 dp[t, eA, eB, eC] = 0;
-                for (i = 0; i < 2; i++)
+                for (int i = 0; i < 2; i++)
                 {
-                    for (j = 0; j < 2; j++)
+                    for (int j = 0; j < 2; j++)
                     {
-                        k = i ^ j;
-                        if ((eA == 0 || i <= (sa[t - 1] - '0')) &&
-                            (eB == 0 || j <= (sb[t - 1] - '0')) &&
-                            (eC == 0 || k <= (sc[t - 1] - '0')))
+                        int k = i ^ j;
+                        if ((eA == 0 || i <= (sa[t + 1] - '0')) &&
+                            (eB == 0 || j <= (sb[t + 1] - '0')) &&
+                            (eC == 0 || k <= (sc[t + 1] - '0')))
                         {
                             dp[t, eA, eB, eC] += solve(
-                                t - 1,
-                                (eA == 1 && i == (sa[t - 1] - '0')) ? 1 : 0,
-                                (eB == 1 && j == (sb[t - 1] - '0')) ? 1 : 0,
-                                (eC == 1 && k == (sc[t - 1] - '0')) ? 1 : 0);
+                                t + 1,
+                                (eA == 1 && i == (sa[t + 1] - '0')) ? 1 : 0,
+                                (eB == 1 && j == (sb[t + 1] - '0')) ? 1 : 0,
+                                (eC == 1 && k == (sc[t + 1] - '0')) ? 1 : 0);
                         }
                     }
                 }
@@ -54,31 +46,26 @@ public class LittleElephantAndXor
 
     public long getNumber(int A, int B, int C)
     {
-        int i = 0, j = 0, m = 0, n = 0;
-        sa = Convert.ToString(A, 2);
-        sb = Convert.ToString(B, 2);
-        sc = Convert.ToString(C, 2);
-        sa = sa.PadLeft(len, '0');
-        sb = sb.PadLeft(len, '0');
-        sc = sc.PadLeft(len, '0');
-
-        sa = reverse(sa);
-        sb = reverse(sb);
-        sc = reverse(sc);
+        sa = Convert.ToString(A, 2).PadLeft(len, '0').ToCharArray();
+        sb = Convert.ToString(B, 2).PadLeft(len, '0').ToCharArray();
+        sc = Convert.ToString(C, 2).PadLeft(len, '0').ToCharArray();
 
         dp = new long[len + 1, 2, 2, 2];
 
-        for (i = 0; i <= len; i++)
+        for (int i = 0; i <= len; i++)
         {
-            for (j = 0; j < 2; j++)
+            for (int j = 0; j < 2; j++)
             {
-                for (m = 0; m < 2; m++)
+                for (int m = 0; m < 2; m++)
                 {
-                    for (n = 0; n < 2; n++) { dp[i, j, m, n] = -1; }
+                    for (int n = 0; n < 2; n++)
+                    {
+                        dp[i, j, m, n] = -1;
+                    }
                 }
             }
         }
 
-        return solve(len, 1, 1, 1);
+        return solve(0, 1, 1, 1);
     }
 }
