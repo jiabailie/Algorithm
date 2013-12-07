764fa694c3787ba6428e6bfbf4b6ab28dc4d7dd6
jiabailie <spotlightyrg@gmail.com>
2013-12-07 19:26:54 +0800

    update file

--- codechef/2013.12/ChefandCodes.cpp
+++ codechef/2013.12/ChefandCodes.cpp
@@ -11,48 +11,48 @@ char sentence[150010];
 
 inline void solve()
 {
-    int cnt[26];
-    int i = 0, j = 25;
-    map<int, vector<char> > dic;
+	int cnt[26];
+	int i = 0, j = 25;
+	map<int, vector<char> > dic;
 	map<char, char> code;
-
-    gets(letter);
+	
+	gets(letter);
 
 	gets(sentence);
 
-    memset(cnt, 0, sizeof(cnt));
-    for(i = 0; i < strlen(sentence); i++)
-    {
-        if(sentence[i] >= 'a' && sentence[i] <= 'z')
-        {
-            cnt[(sentence[i] - 'a')]++;
-        }
+	memset(cnt, 0, sizeof(cnt));
+	for(i = 0; i < strlen(sentence); i++)
+	{
+		if(sentence[i] >= 'a' && sentence[i] <= 'z')
+		{
+			cnt[(sentence[i] - 'a')]++;
+		}
 		else if(sentence[i] >= 'A' && sentence[i] <= 'Z')
 		{
 			cnt[(sentence[i] - 'A')]++;
 		}
-    }
+	}
 
-    for(i = 0; i < 26; i++)
-    {
+	for(i = 0; i < 26; i++)
+	{
 		if(cnt[i] > 0)
 		{
 			dic[cnt[i]].push_back(char('a' + i));
 		}
-    }
+	}
 
 	for(map<int, vector<char> >::reverse_iterator it = dic.rbegin(); it != dic.rend(); it++)
-    {
-        for(vector<char>::reverse_iterator iit = it->second.rbegin(); iit != it->second.rend(); iit++)
-        {
+	{
+		for(vector<char>::reverse_iterator iit = it->second.rbegin(); iit != it->second.rend(); iit++)
+		{
 			code[(*iit)] = letter[j--];
-        }
-    }
+		}
+	}
 
 	for(i = 0; i < strlen(sentence); i++)
 	{
 		if(sentence[i] >= 'a' && sentence[i] <= 'z')
-        {
+		{
 			sentence[i] = code[sentence[i]];
 		}
 		else if(sentence[i] >= 'A' && sentence[i] <= 'Z')
@@ -66,14 +66,14 @@ inline void solve()
 
 int main()
 {
-    int t = 0;
-    int i = 0;
+	int t = 0;
+	int i = 0;
 
-    scanf("%d", &t);
+	scanf("%d", &t);
 	getchar();
-    for(i = 0; i < t; i++)
-    {
-        solve();
-    }
-    return 0;
+	for(i = 0; i < t; i++)
+	{
+		solve();
+	}
+	return 0;
 }
