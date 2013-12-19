f7d87d31b73c06149e6adceedd11f2fbc70bd369
Ruiguo Yang <spotlightyrg@gmail.com>
2013-12-19 11:21:08 +0800

    Update Match82.cpp

--- marathon/Match82.cpp
+++ marathon/Match82.cpp
@@ -133,6 +133,12 @@ public:
 	// judge whether current point's top/left/bottom/right positions have same color points.
 	inline bool judge(const point&); 
 
+	// get how many colors certain cell is painted.
+	inline int getcolorkinds(const point&);
+
+	// calculate the penalty of certain point.
+	inline int calpointpenalty(const point&);
+
 	// find the nearest point which has same color.
 	inline void find(int, int&, int&, const point&); 
 
@@ -143,14 +149,22 @@ public:
 	// merge two different connected-components.
 	inline void mergecc(component&, component&);
 
+	// calculate the penalty from one point a to another b
+	// there is a middle point c, if c(-1, -1) a and b are co-linear.
+	inline int calPenalty(point&, point&, point&);
+
+	// calculate the penalty from a to b
+	// a and b are co-linear.
+	inline int calLinePenalty(point&, point&);
+
 	// find nearest distance between two connected-components.
-	inline int calCcDis(point&, point&, component&, component&);
+	inline int calCcDis(point&, point&, point&, component&, component&);
 
 	// do breadth first search to find how many discrete connected-component in this image
 	inline component bfs(int, point&);
 
 	// paint the path from point A to point B using color.
-	inline void paint(int, point&, point&, component&); 	
+	inline void paint(int, point&, point&, point&, component&); 	
 	
 	// paint the line from point A to point B using color.
 	inline void paintline(int, point&, point&, component&); 
@@ -214,6 +228,26 @@ inline bool ColorLinker::judge(const point& p)
 	return ret;
 }
 
+inline int ColorLinker::getcolorkinds(const point& p)
+{
+	int k = 0;
+	int c = grid[p.x][p.y];
+
+	while(c != 0)
+	{
+		k++;
+		c &= (c - 1);
+	}
+	return k;
+}
+
+inline int ColorLinker::calpointpenalty(const point& p)
+{
+	int k = getcolorkinds(p);
+
+	return k + k * (k - 1) * penalty;
+}
+
 inline void ColorLinker::find(int color, int& fpx, int& fpy, const point& p)
 {
 	int i = 0;
@@ -261,22 +295,81 @@ inline void ColorLinker::mergecc(component& c1, component& c2)
 	}
 }
 
-inline int ColorLinker::calCcDis(point& p1, point& p2, component& c1, component& c2)
+inline int ColorLinker::calPenalty(point& a, point& middle, point& b)
+{
+	int ret = 0;
+
+	if(a.x != b.x && a.y != b.y)
+	{
+		point m1(a.x, b.y);
+		point m2(b.x, a.y);
+
+		int penalty1 = calLinePenalty(a, m1) + calLinePenalty(m1, b) + calpointpenalty(m1);
+		int penalty2 = calLinePenalty(a, m2) + calLinePenalty(m2, b) + calpointpenalty(m2);
+
+		middle = m1;
+		ret = penalty1;
+		if(penalty1 > penalty2)
+		{
+			middle = m2;
+			ret = penalty2;
+		}
+	}
+	else
+	{
+		middle = point(-1, -1);
+		ret = calLinePenalty(a, b);
+	}
+	return ret;
+}
+
+inline int ColorLinker::calLinePenalty(point& a, point& b)
+{
+	int i = 0;
+	int s = 0, e = 0;
+	int ret = 0;
+
+	if(a.x == b.x)
+	{
+		s = imin(a.y, b.y);
+		e = imax(a.y, b.y);
+
+		for(i = s + 1; i < e; i++)
+		{
+			ret += calpointpenalty(point(a.x, i));
+		}
+	}
+	else if(a.y == b.y)
+	{
+		s = imin(a.x, b.x);
+		e = imax(a.x, b.x);
+
+		for(i = s + 1; i < e; i++)
+		{
+			ret += calpointpenalty(point(i, a.y));
+		}
+	}
+	return ret;
+}
+
+inline int ColorLinker::calCcDis(point& p1, point& middle, point& p2, component& c1, component& c2)
 {
 	int i = 0, j = 0;
 	int tmpDis = 0;
-	int minDis = MAX_DIS;
+	int minDis = INT_MAX;
+	point tmiddle(-1, -1);
 
 	for(i = 0; i < c1.points.size(); i++)
 	{
 		for(j = 0; j < c2.points.size(); j++)
 		{
-			tmpDis = manhattanDis(c1.points[i], c2.points[j]);
+			tmpDis = calPenalty(c1.points[i], tmiddle, c2.points[j]);
 			if(tmpDis < minDis)
 			{
 				minDis = tmpDis;
 				p1 = c1.points[i];
 				p2 = c2.points[j];
+				middle = tmiddle;
 			}
 		}
 	}
@@ -321,30 +414,29 @@ inline component ColorLinker::bfs(int color, point& p)
 	return segment;
 }
 
-inline void ColorLinker::paint(int color, point& a, point& b, component& cc)
+inline void ColorLinker::paint(int color, point& a, point& middle, point& b, component& cc)
 {
 	int i = 0;
 
-	if(a.x == b.x || a.y == b.y)
+	if(middle.x == -1 && middle.y == -1)
 	{
 		paintline(color, a, b, cc);
 	}
 	else
 	{
-		point c(b.x, a.y);
-		grid[b.x][a.y] |= csign[color];
-		statis[color].push_back(c);
+		grid[middle.x][middle.y] |= csign[color];
+		statis[color].push_back(middle);
 
-		cc.add(c);
+		cc.add(middle);
 
 		cnt += 3;
 
-		ret.push_back(b.x);
-		ret.push_back(a.y);
+		ret.push_back(middle.x);
+		ret.push_back(middle.y);
 		ret.push_back(color);
 
-		paintline(color, a, c, cc);
-		paintline(color, c, b, cc);
+		paintline(color, a, middle, cc);
+		paintline(color, middle, b, cc);
 	}
 }
 
@@ -411,9 +503,11 @@ inline void ColorLinker::adjust(int color)
 
 	point startPoint;
 	point endPoint;
+	point middle(-1 ,-1);
 
 	point tsPoint;
 	point tePoint;
+	point tmiddle(-1 ,-1);
 
 	vector<component> components = findcc(color);
 
@@ -425,12 +519,13 @@ inline void ColorLinker::adjust(int color)
 		{
 			for(j = i + 1; j < components.size(); j++)
 			{
-				tmpDis = calCcDis(tsPoint, tePoint, components[i], components[j]);
+				tmpDis = calCcDis(tsPoint, tmiddle, tePoint, components[i], components[j]);
 				if(tmpDis < minDis)
 				{
 					minDis = tmpDis;
 					startPoint = tsPoint;
 					endPoint = tePoint;
+					middle = tmiddle;
 					lastI = i;
 					lastJ = j;
 				}
@@ -439,12 +534,10 @@ inline void ColorLinker::adjust(int color)
 
 		mergecc(components[lastI], components[lastJ]);
 
-		paint(color, startPoint, endPoint, components[lastI]);
+		paint(color, startPoint, middle, endPoint, components[lastI]);
 
 		components.erase(components.begin() + lastJ);
 	}
-
-	i = 0;
 }
 
 vector<int> ColorLinker::link(vector<string> board, int ipenalty)
@@ -452,6 +545,8 @@ vector<int> ColorLinker::link(vector<string> board, int ipenalty)
 	int i = 0;
 	int j = 0;
 	int c = 0;
+
+	map<int, vector<int> > order;
 	
 	penalty = ipenalty;
 	gridSize = board.size();		
@@ -480,7 +575,15 @@ vector<int> ColorLinker::link(vector<string> board, int ipenalty)
 
 	for(map<int, vector<point> >::iterator it = statis.begin(); it != statis.end(); it++)
 	{
-		adjust(it->first);
+		order[it->second.size()].push_back(it->first);
+	}
+
+	for(map<int, vector<int> >::reverse_iterator it = order.rbegin(); it != order.rend(); it++)
+	{
+		for(i = 0; i < it->second.size(); i++)
+		{
+			adjust(it->second[i]);
+		}
 	}
 
 	return ret;
@@ -596,9 +699,9 @@ int main()
 	{
 		for(vector<point>::iterator it = imap->second.begin(); it != imap->second.end(); it++)
 		{
-			fout << it->x << " "
-				 << it->y << " "
-				 << imap->first << endl;
+			fout << it->x << " ";
+			fout << it->y << " ";
+			fout << imap->first << endl;
 		}
 	}	
 
@@ -611,9 +714,9 @@ int main()
 	{
 		for(vector<point>::iterator it = imap->second.begin(); it != imap->second.end(); it++)
 		{
-			cout << it->x << endl
-				 << it->y << endl
-				 << imap->first << endl;
+			cout << it->x << endl;
+			cout << it->y << endl;
+			cout << imap->first << endl;
 		}
 	}	
 #endif
