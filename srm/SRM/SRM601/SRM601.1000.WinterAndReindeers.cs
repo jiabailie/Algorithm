fb7669871b077fd69e8f4d2185bdd5c3a7060d36
Ruiguo Yang <spotlightyrg@gmail.com>
2013-12-25 09:40:35 +0800

    Update SRM601.1000.WinterAndReindeers.cs

--- srm/SRM/SRM601/SRM601.1000.WinterAndReindeers.cs
+++ srm/SRM/SRM601/SRM601.1000.WinterAndReindeers.cs
@@ -4,106 +4,105 @@ using System.Text;
 
 public class WinterAndReindeers
 {
-    private string a = null;
-    private string b = null;
-    private string c = null;
-    private int[,] dp = null;
-
-    private string composestring(string[] ss)
+    private int[] findpos(string x, string y)
     {
-        StringBuilder sb = new StringBuilder();
-        foreach (string s in ss)
-        {
-            sb.Append(s);
-        }
-        return sb.ToString();
-    }
-
-    private int lcs(string x, string y)
-    {
-        if (x.Length * y.Length == 0) { return 0; }
-
-        int i = 0, j = 0;
-        dp = new int[x.Length + 1, y.Length + 1];
-        for (i = 0; i < x.Length; i++)
-        {
-            for (j = 0; j < y.Length; j++)
-            {
-                if (x[i] == y[j])
-                {
-                    dp[i + 1, j + 1] = dp[i, j] + 1;
-                }
-                else
-                {
-                    dp[i + 1, j + 1] = Math.Max(dp[i + 1, j], dp[i, j + 1]);
-                }
-            }
-        }
-
-        return dp[x.Length, y.Length];
-    }
-
-    private Dictionary<int, int> getpos(string x, string y)
-    {
-        int i = 0, j = 0, k = 0;
-        Dictionary<int, int> pos = new Dictionary<int, int>();
+        bool ok = false;
+        int i = 0, j = 0, pos = 0;
+        int[] position = new int[x.Length + 7];
 
         for (i = 0; i < x.Length; i++)
         {
-            if (x[i] == y[0])
+            pos = i;
+            ok = true;
+
+            for (j = 0; j < y.Length && ok; j++)
             {
-                j = i;
-                k = 0;
-                while (j < x.Length && k < y.Length)
-                {
-                    for (; j < x.Length && x[j] != y[k]; j++) ;
-                    j++;
-                    k++;
-                }
-                if (k == y.Length)
-                {
-                    pos.Add(i, j);
-                }
+                while (pos < x.Length && y[j] != x[pos]) { pos++; }
+                if (pos == x.Length) { ok = false; }
+                if (pos < x.Length) { pos++; }
             }
+            if (!ok) { pos = x.Length; }
+            else { pos--; }
+            position[i] = pos;
         }
-        return pos;
+        return position;
     }
 
     public int getNumber(string[] allA, string[] allB, string[] allC)
     {
-        int ret = 0, max = int.MinValue;
-        int k = 0, l = 0;
-        a = composestring(allA);
-        b = composestring(allB);
-        c = composestring(allC);
-        if (c.Length > a.Length || c.Length > b.Length) { return 0; }
+        int i = 0, j = 0;
+        int ii = 0, jj = 0;
+        int a = 0, b = 0, c = 0;
+        int res = 0;
+        string sa = "", sb = "", sc = "";
 
-        if (lcs(a, c) == c.Length && lcs(b, c) == c.Length)
+        foreach (string s in allA) { sa += s; }
+        foreach (string s in allB) { sb += s; }
+        foreach (string s in allC) { sc += s; }
+
+        a = sa.Length;
+        b = sb.Length;
+        c = sc.Length;
+
+        int[] apos = findpos(sa, sc);
+        int[] bpos = findpos(sb, sc);
+
+        int[,] dleft = new int[a + 7, b + 7];
+        int[,] dright = new int[a + 7, b + 7];
+
+        for (i = 0; i <= a; i++)
         {
-            l = lcs(a, b);
-            Dictionary<int, int> apos = getpos(a, c);
-            Dictionary<int, int> bpos = getpos(b, c);
-
-            foreach (KeyValuePair<int, int> akvp in apos)
+            for (j = 0; j <= b; j++)
             {
-                foreach (KeyValuePair<int, int> bkvp in bpos)
+                if (i == 0 || j == 0)
                 {
-                    k = dp[akvp.Key, bkvp.Key];
-                    if (akvp.Value <= a.Length && bkvp.Value <= b.Length)
+                    dleft[i, j] = 0;
+                }
+                else
+                {
+                    dleft[i, j] = 0;
+                    dleft[i, j] = Math.Max(dleft[i, j], dleft[i - 1, j]);
+                    dleft[i, j] = Math.Max(dleft[i, j], dleft[i, j - 1]);
+                    if (sa[i - 1] == sb[j - 1])
                     {
-                        k += l - dp[akvp.Value, bkvp.Value];
-                    }
-
-                    if (k > max)
-                    {
-                        max = k;
+                        dleft[i, j] = Math.Max(dleft[i, j], dleft[i - 1, j - 1] + 1);
                     }
                 }
             }
-
-            ret = c.Length + max;
         }
 
-        return ret;
+        for (i = a; i >= 0; i--)
+        {
+            for (j = b; j >= 0; j--)
+            {
+                if (i == a || j == b)
+                {
+                    dright[i, j] = 0;
+                }
+                else
+                {
+                    dright[i, j] = 0;
+                    dright[i, j] = Math.Max(dright[i, j], dright[i + 1, j]);
+                    dright[i, j] = Math.Max(dright[i, j], dright[i, j + 1]);
+                    if (sa[i] == sb[j])
+                    {
+                        dright[i, j] = Math.Max(dright[i, j], dright[i + 1, j + 1] + 1);
+                    }
+                }
+            }
+        }
+
+        for (i = 0; i < a; i++)
+        {
+            for (j = 0; j < b; j++)
+            {
+                ii = apos[i];
+                jj = bpos[j];
+                if (ii == a || jj == b) { continue; }
+                res = Math.Max(res, dleft[i, j] + dright[ii + 1, jj + 1] + c);
+            }
+        }
+
+        return res;
     }
 }
