83a90186d260d30f831a45cc9b272d38c214d888
jiabailie <spotlightyrg@gmail.com>
2014-01-01 22:40:36 +0800

    SRM601

---
+++ srm/SRM/SRM602/SRM602.250.TypoCoderDiv2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class TypoCoderDiv2
+{
+    public int count(int[] rating)
+    {
+        int i = 0;
+        int cnt = 0;
+        bool brown = true;
+        bool ciel = false;
+        bool pre = ciel;
+        bool cur = false;
+
+        for (i = 0; i < rating.Length; i++)
+        {
+            cur = ciel;
+            if (rating[i] >= 1200)
+            {
+                cur = brown;
+            }
+            if ((cur ^ pre))
+            {
+                cnt++;
+            }
+            pre = cur;
+        }
+        return cnt;
+    }
+}
