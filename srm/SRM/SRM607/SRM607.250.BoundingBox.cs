93f816b8f6650c1e152c5d99b4fff469eecbc1bc
rgyang <rgyang@ctrip.com>
2014-02-19 20:23:59 +0800

    add srm607

---
+++ srm/SRM/SRM607/SRM607.250.BoundingBox.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class BoundingBox
+{
+    public int smallestArea(int[] X, int[] Y)
+    {
+        int minX = int.MaxValue;
+        int minY = int.MaxValue;
+        int maxX = int.MinValue;
+        int maxY = int.MinValue;
+
+        foreach (int x in X)
+        {
+            if (x < minX) { minX = x; }
+            if (x > maxX) { maxX = x; }
+        }
+        foreach (int y in Y)
+        {
+            if (y < minY) { minY = y; }
+            if (y > maxY) { maxY = y; }
+        }
+
+        return (maxX - minX) * (maxY - minY);
+    }
+}
---
+++ srm/SRM/SRM607/SRM607.500.PalindromicSubstringsDiv2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+
+public class PalindromicSubstringsDiv2
+{
+    public int count(string[] S1, string[] S2)
+    {
+        int ret = 0;
+        int i = 0, j = 0;
+        int m = 0, n = 0, e = 0;
+        StringBuilder sb = new StringBuilder();
+        string all = "";
+        foreach (string s in S1) { sb.Append(s); }
+        foreach (string s in S2) { sb.Append(s); }
+
+        all = sb.ToString();
+        n = all.Length;
+
+        for (m = 0; m < n; ++m)
+        {
+            for (e = 0; e < 2; ++e)
+            {
+                if (e == 0) // even
+                {
+                    i = m;
+                    j = m + 1;
+                }
+                else // odd
+                {
+                    i = m - 1;
+                    j = m + 1;
+                    ++ret;
+                }
+                bool p = true;
+                while (i >= 0 && j < n && p)
+                {
+                    p = p & (all[i] == all[j]);
+                    if (p) { ++ret; }
+                    i--;
+                    j++;
+                }
+            }
+        }
+        return ret;
+    }
+}
