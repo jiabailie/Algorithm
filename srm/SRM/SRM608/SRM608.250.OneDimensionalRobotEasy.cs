965d5090bfdf08f31fa946fc837edc82ff733b22
rgyang <rgyang@ctrip.com>
2014-02-22 15:06:54 +0800

    update srm, add srm 608

---
+++ srm/SRM/SRM608/SRM608.250.OneDimensionalRobotEasy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class OneDimensionalRobotEasy
+{
+    public int finalPosition(string commands, int A, int B)
+    {
+        int pos = 0;
+
+        A = -A;
+
+        foreach (char c in commands)
+        {
+            if (c == 'L')
+            {
+                if (pos == A) { continue; }
+                pos -= 1;
+            }
+            else if (c == 'R')
+            {
+                if (pos == B) { continue; }
+                pos += 1;
+            }
+        }
+        return pos;
+    }
+}
---
+++ srm/SRM/SRM608/SRM608.500.MysticAndCandiesEasy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public class MysticAndCandiesEasy
+{
+    public int minBoxes(int C, int X, int[] high)
+    {
+        int sum = 0, b = 0;
+        int t = 0, n = high.Length;
+
+        foreach (int x in high) { sum += x; }
+
+        Array.Sort(high);
+
+        for (t = 1; t < high.Length; ++t)
+        {
+            b += high[n - t];
+            if (C - sum + b >= X)
+            {
+                return t;
+            }
+        }
+        return high.Length;
+    }
+}
