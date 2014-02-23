0be796462f5cf571cb1113391543aef5e2d1273d
rgyang <rgyang@ctrip.com>
2014-02-23 11:22:22 +0800

    add srm 609

---
+++ srm/SRM/SRM609/SRM609.250.MagicalStringDiv2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class MagicalStringDiv2
+{
+    public int minimalMoves(string S)
+    {
+        int n = S.Length;
+        int i = 0, cnt = 0;
+
+        for (i = 0; i < n / 2; ++i)
+        {
+            if (S[i] == '<') { ++cnt; }
+        }
+        for (i = n / 2; i < n; ++i)
+        {
+            if (S[i] == '>') { ++cnt; }
+        }
+        return cnt;
+    }
+}
---
+++ srm/SRM/SRM609/SRM609.500.PackingBallsDiv2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public class PackingBallsDiv2
+{
+    public int minPacks(int R, int G, int B)
+    {
+        int res = 100;
+        int i = 0, j = 0;
+        int t = 0, r = 0;
+        int[] p = new int[] { R, G, B };
+
+        for (i = 0; i < 100; ++i)
+        {
+            t = i;
+            for (j = 0; j < 3; ++j)
+            {
+                r = Math.Max(0, p[j] - i);
+                t += (r + 2) / 3;
+            }
+            res = Math.Min(res, t);
+        }
+        return res;
+    }
+}
