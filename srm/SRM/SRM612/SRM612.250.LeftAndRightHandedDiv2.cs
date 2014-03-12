ce3370553d470145f06ba92ed6ed6862d51ec0a2
rgyang <rgyang@ctrip.com>
2014-03-12 10:43:22 +0800

    add srm 612

---
+++ srm/SRM/SRM612/SRM612.250.LeftAndRightHandedDiv2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class LeftAndRightHandedDiv2
+{
+    public int count(string S)
+    {
+        int i = 0;
+        int cnt = 0;
+        for (i = 0; i < S.Length - 1; ++i)
+        {
+            if (S[i] == 'R' && S[i + 1] == 'L')
+            {
+                cnt += 1;
+            }
+        }
+        return cnt;
+    }
+}
---
+++ srm/SRM/SRM612/SRM612.500.EmoticonsDiv2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class EmoticonsDiv2
+{
+    private int Smiles = 0;
+    private int p = 0;
+
+    private void step(int clipboard, int ops, int window)
+    {
+        if (window >= Smiles)
+        {
+            if (window == Smiles)
+            {
+                p = Math.Min(p, ops);
+            }
+            return;
+        }
+
+        // copy
+        if (window > clipboard)
+        {
+            step(window, ops + 1, window);
+        }
+
+        // paste
+        if (clipboard > 0)
+        {
+            step(clipboard, ops + 1, window + clipboard);
+        }
+    }
+
+    public int printSmiles(int smiles)
+    {
+        Smiles = smiles;
+        p = 2000;
+        step(0, 0, 1);
+
+        return p;
+    }
+}
