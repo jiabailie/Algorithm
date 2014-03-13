81b83b67fb64e37b768994d1fb6fbbc200b7df95
rgyang <rgyang@ctrip.com>
2014-03-13 18:20:40 +0800

    add 14031302.cpp

---
+++ daily-practice/2014/14031302.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class TextBlock {  
+    public: 
+		TextBlock(string s)
+		{
+			text = string(s.begin(), s.end());
+		}
+        const char& operator[] (std::size_t position) const  
+        {
+			return text[position];
+		}  
+        char& operator[] (std::size_t position)  
+        {
+			return text[position];
+		}  
+    private:  
+        string text;  
+};
+
+int main()
+{
+    TextBlock tb("Hello");  
+    std::cout << tb[0];  
+       
+    const TextBlock ctb("World");  
+    std::cout << ctb[0];  
+
+	return 0;
+}
