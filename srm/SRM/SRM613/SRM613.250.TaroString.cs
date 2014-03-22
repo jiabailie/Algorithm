cbc6225e1555ddbc77093eea6e1cc27dda02e4e0
jiabailie <spotlightyrg@gmail.com>
2014-03-22 10:18:31 +0800

    add srm 613

---
+++ srm/SRM/SRM613/SRM613.250.TaroString.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class TaroString
+{
+    public string getAnswer(string S)
+    {
+        int i = 0, j = 0;
+        int[] cnt = new int[3];
+        int[] pos = new int[3];
+        string source = "CAT";
+        for (; i < S.Length; ++i)
+        {
+            for (j = 0; j < 3; ++j)
+            {
+                if (S[i] == source[j])
+                {
+                    cnt[j] += 1;
+                    pos[j] = i;
+                }
+            }
+        }
+        if (!(pos[0] < pos[1] && pos[1] < pos[2]))
+        {
+            return "Impossible";
+        }
+        for (i = 0; i < 3; ++i)
+        {
+            if (cnt[i] != 1)
+            {
+                return "Impossible";
+            }
+        }
+        return "Possible";
+    }
+}
---
+++ srm/SRM/SRM613/SRM613.500.TaroFriends.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+public class TaroFriends
+{
+    public int getNumber(int[] coordinates, int X)
+    {
+        int i = 0, j = 0;
+        int min = 0, max = 0;
+        int n = coordinates.Length;
+        int[] r = new int[n];
+        int[] l = new int[n];
+        int ret = int.MaxValue;
+
+        Array.Sort(coordinates);
+        for (i = 0; i < n; ++i)
+        {
+            r[i] = coordinates[i] + X;
+            l[i] = coordinates[i] - X;
+        }
+
+        for (i = 0; i < n; ++i)
+        {
+            min = int.MaxValue;
+            max = int.MinValue;
+            for (j = 0; j < i; ++j)
+            {
+                if (r[j] < min)
+                {
+                    min = r[j];
+                }
+                if (r[j] > max)
+                {
+                    max = r[j];
+                }
+            }
+            for (j = i; j < n; ++j)
+            {
+                if (l[j] < min)
+                {
+                    min = l[j];
+                }
+                if (l[j] > max)
+                {
+                    max = l[j];
+                }
+            }
+
+            if (max - min < ret)
+            {
+                ret = max - min;
+            }
+        }
+        return ret;
+    }
+}
